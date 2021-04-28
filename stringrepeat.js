6d325f494d459051675afe9a1e0d6f593982501a
Cayla Franklin <cjfranklin4@gmail.com>
2021-04-27 20:51:51 -0500

    Create stringrepeat.js
    New CodeWars Kata

---
+++ stringrepeat.js
@@ -0,0 +1,7 @@
+/*
+Write a function called repeatStr which repeats the given string string exactly n times.
+*/
+function repeatStr (n, s) {
+  let repeatString = s.repeat(n)
+  return repeatString;
+}
