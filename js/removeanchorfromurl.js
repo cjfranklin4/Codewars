5fca6b24f9f5c68ef05d71cd949d3995d8355030
Cayla Franklin <cjfranklin4@gmail.com>
2021-07-13 12:50:28 -0500

    new codewar

---
+++ js/removeanchorfromurl.js
@@ -0,0 +1,9 @@
+/*
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+*/
+
+function removeUrlAnchor(url){
+    // TODO: complete
+    let remove = url.split('#')
+    return remove[0]
+  }
