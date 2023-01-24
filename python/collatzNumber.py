b7d3304743037c91a47927f97c40e938719a0ab0
Cayla Franklin <cjfranklin4@gmail.com>
2023-01-23 20:49:58 -0600

    daily codewars python

---
+++ python/collatzNumber.py
@@ -0,0 +1,16 @@
+#Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value. If number is odd, then collatz() should print and return 3 * number + 1.
+#Then write a program that lets the user type in an integer and that keeps calling collatz() on that number until the function returns the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the _Collatz sequence_, sometimes called “the simplest impossible math problem.”)
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        print(3*number + 1)
+        return 3*number + 1
+
+print('enter a number:')
+inputNum = int(input())
+while inputNum != 1:
+    colNum = collatz(inputNum)
+    inputNum = colNum
